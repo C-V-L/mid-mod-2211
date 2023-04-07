565a000f046f4e414abf11fc24b65206455c1778
Conner Van Loan <cdvanloan@gmail.com>
2023-04-07 10:12:08 -0600

    edits facade spec search params to build more robust results

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe FoodFacade do
   describe 'instance methods' do
     describe '#total_results' do
       it 'returns the total number of results' do
-        facade = FoodFacade.new('sweet potatoes')
+        facade = FoodFacade.new('cheese')
 
-        expect(facade.total_results).to eq(51270)
+        expect(facade.total_results).to eq(56899)
       end
     end
 
     describe '#food_list' do
       it 'returns a list of 10 foods for a given search' do
-        facade = FoodFacade.new('sweet potatoes')
+        facade = FoodFacade.new('cheese')
         expect(facade.food_list).to be_an(Array)
         expect(facade.food_list.first).to be_a(Food)
         expect(facade.food_list.count).to eq(10)
